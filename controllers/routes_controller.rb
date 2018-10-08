3a80bbb0660e313fe20bbc1b17a7255100d085b2
Graham-Stein <graham@guavoaviation.co.uk>
2018-10-08 20:25:11 +0100

    Add functioning new and create for routes. Need to set default values of nil for summer and winter grades.

--- controllers/routes_controller.rb
+++ controllers/routes_controller.rb
@@ -9,3 +9,13 @@ get ('/routes') do
   @routes = Route.all()
   erb(:"routes/index")
 end
+
+get ('/routes/new') do
+  @crags = Crag.all()
+  erb(:"routes/new")
+end
+
+post ('/routes/') do
+  Route.new(params).save
+  redirect to '/routes'
+end
